7e8bbf11474f99f2f4e483a31a7b3b5e80f06e99
Alsey Coleman Miller <alseycmiller@gmail.com>
2013-10-20 15:13:53 -0400

    inbox is functional

--- Facebook Messenger/Facebook Messenger/FBMInboxWindowController.h
+++ Facebook Messenger/Facebook Messenger/FBMInboxWindowController.h
@@ -8,6 +8,27 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface FBMInboxWindowController : NSWindowController
+@interface FBMInboxWindowController : NSWindowController <NSTableViewDataSource, NSTableViewDelegate>
+{
+    NSMutableArray *_conversations;
+    
+    NSDateFormatter *_dateFormatter;
+}
+
+@property (weak) IBOutlet NSTableView *tableView;
+
+-(void)refreshConversationsFromNet;
+
+-(void)refreshConversationsFromCache;
+
+#pragma mark - First Responder
+
+-(IBAction)newDocument:(id)sender;
+
+-(IBAction)delete:(id)sender;
+
+#pragma mark
+
+
 
 @end
--- Facebook Messenger/Facebook Messenger/FBMInboxWindowController.m
+++ Facebook Messenger/Facebook Messenger/FBMInboxWindowController.m
@@ -9,6 +9,8 @@
 #import "FBMInboxWindowController.h"
 #import "FBMAppDelegate.h"
 #import "FBMStore.h"
+#import "FBConversation.h"
+#import "FBUser.h"
 
 @interface FBMInboxWindowController ()
 
@@ -38,11 +40,125 @@
     
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     
-    
+    [self refreshConversationsFromCache];
 }
 
-#pragma mark
+#pragma mark - Refresh
 
+-(void)refreshConversationsFromCache
+{
+    FBMAppDelegate *appDelegate = [NSApp delegate];
+    
+    [appDelegate.store.context performBlockAndWait:^{
+       
+        NSFetchRequest *fetch = [NSFetchRequest fetchRequestWithEntityName:@"FBConversation"];
+        
+        NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"updatedTime"
+                                                                         ascending:NO];
+        
+        fetch.sortDescriptors = @[sortDescriptor];
+        
+        NSError *fetchError;
+        
+        NSArray *result = [appDelegate.store.context executeFetchRequest:fetch
+                                                                   error:&fetchError];
+        
+        if (!result) {
+            
+            [NSException raise:@"Error Executing Core Data NSFetchRequest"
+                        format:@"%@", fetchError.localizedDescription];
+            
+            return;
+        }
+        
+        _conversations = [[NSMutableArray alloc] initWithArray:result];
+        
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+           
+            [self.tableView reloadData];
+            
+        }];
+    }];
+}
+
+-(void)refreshConversationsFromNet
+{
+    FBMAppDelegate *appDelegate = [NSApp delegate];
+    
+    [appDelegate.store requestInboxWithCompletionBlock:^(NSError *error) {
+       
+        if (error) {
+            
+            [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                
+                [NSApp presentError:error];
+            }];
+            
+            return;
+        }
+        
+        [self refreshConversationsFromCache];
+        
+    }];
+}
+
+#pragma mark - NSTableView DataSource
+
+-(NSInteger)numberOfRowsInTableView:(NSTableView *)tableView
+{
+    return _conversations.count;
+}
+
+#pragma mark - NSTableView Delegate
+
+-(NSView *)tableView:(NSTableView *)tableView
+  viewForTableColumn:(NSTableColumn *)tableColumn
+                 row:(NSInteger)row
+{
+    NSString *identifier = tableColumn.identifier;
+    
+    NSTableCellView *cellView = [self.tableView makeViewWithIdentifier:identifier
+                                                                 owner:self];
+    
+    // get model object
+    FBConversation *conversation = _conversations[row];
+    
+    if ([identifier isEqualToString:@"updatedTime"]) {
+        
+        if (!_dateFormatter) {
+            
+            _dateFormatter = [[NSDateFormatter alloc] init];
+            _dateFormatter.dateStyle = NSDateFormatterMediumStyle;
+        }
+        
+        // set date text
+        cellView.textField.stringValue = [_dateFormatter stringFromDate:conversation.updatedTime];
+        
+    }
+    
+    if ([identifier isEqualToString:@"to"]) {
+        
+        // build string
+        
+        NSArray *toArray = conversation.to.allObjects;
+        
+        NSString *toString = @"";
+        for (FBUser *user in toArray) {
+            
+            toString = [toString stringByAppendingString:user.name];
+            
+            if (user != toArray.lastObject) {
+                
+                toString = [toString stringByAppendingString:@", "];
+            }
+        }
+        
+        // set text
+        cellView.textField.stringValue = toString;
+    }
+    
+    return cellView;
+}
 
 
 @end
